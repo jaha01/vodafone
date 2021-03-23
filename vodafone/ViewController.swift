142b30be532062b139beb0c9789c5080177bac2d
Jahongir <jahongir@MacBook-Pro-Gulbahor.local>
2021-03-23 23:28:00 +0300

    commit-22.03.21

---
+++ vodafone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vodafone
+//
+//  Created by Jahongir on 1/15/21.
+//  Copyright © 2021 Jahongir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
